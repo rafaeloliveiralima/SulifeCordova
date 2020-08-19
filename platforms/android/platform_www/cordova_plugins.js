7d31638e7aed6305aaa3c856e286df677820d078
Rafael Lima <rafaeloliveiralima@ibest.com.br>
2020-08-19 12:06:48 -0300

    update 4

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -409,13 +409,13 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-geolocation": "4.0.2",
     "cordova-plugin-globalization": "1.11.0",
     "cordova-plugin-inappbrowser": "4.0.0",
+    "cordova-plugin-ios-camera-permissions": "1.2.0",
     "cordova-plugin-media": "5.0.3",
     "cordova-plugin-media-capture": "3.0.3",
     "cordova-plugin-network-information": "2.0.2",
     "cordova-plugin-splashscreen": "6.0.0",
     "cordova-plugin-statusbar": "2.4.3",
     "cordova-plugin-vibration": "3.1.1",
-    "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-ios-camera-permissions": "1.2.0"
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
